7b98cc9b4311a64b791b65b22bfd223392dd38ca
gidoko-codes <g.idoko@alustudent.com>
2026-07-25 17:30:37 +0200

    Grace: Welcome message

--- main.py
+++ main.py
@@ -37,3 +37,19 @@ def analyze_students():
     print(f"Total Number of Students : {total_students}")
     print(f"Good Standing            : {good_standing_count}")
     print(f"Needs Improvement        : {needs_improvement_count}")
+
+
+#Welcome screen and main menu (Option display)
+def print_menu():
+    print("""==========================================
+  Student Online Record-Keeping System
+==========================================
+
+Welcome to the Student Online Record-Keeping System!
+
+Please choose an option:
+
+1. Register a Student
+2. View Student Records
+3. Analyse Student Records
+4. Exit""")
